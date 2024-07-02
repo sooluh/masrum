ccbedd70d1f381dc963579c0e9dbaee57808c404
Abu Masyail <20874779+sooluh@users.noreply.github.com>
2024-07-02 21:26:14 +0700

    chore: annoying initial project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,6 +20,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Jamurize"
+rootProject.name = "Masrum"
 include(":app")
- 
